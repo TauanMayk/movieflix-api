3d565ea2e9dd0a25fbcbb9ec1831996d6a9b10e5
Tauan Mayk <tauanmaykreal@gmail.com>
2025-05-14 20:12:13 -0300

    atualizando com o put e validando com o findFirst

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
+import { Request, Response } from "express";
 
 const prisma = new PrismaClient();
 const port = 3000;
 const app = express();
-app.use(express.json())
 
+app.use(express.json());
 
 app.get("/movies", async (_, res) => {
   const movies = await prisma.movie.findMany({
@@ -20,23 +21,49 @@ app.get("/movies", async (_, res) => {
   res.json(movies);
 });
 
-
 app.post("/movies", async (req, res) => {
-
-  console.log(req.body);
-
   const { title, genre_id, language_id, oscar_count, release_date } = req.body;
-
-  await prisma.movie.create({
-    data: {
-      title: title, 
-      genre_id: genre_id,
-      language_id: language_id,
-      oscar_count: oscar_count,
-      release_date: new Date(release_date),
-    },
-  });
+  try {
+    const movieWithSameTitle = await prisma.movie.findFirst({
+      where: {
+        title: { equals: title, mode: "insensitive" },
+      },
+    });
+    if (movieWithSameTitle) {
+      res.status(409).send({ message: "Já existe um filme com esse título" });
+    }
+    await prisma.movie.create({
+      data: {
+        title,
+        genre_id,
+        language_id,
+        oscar_count,
+        release_date: new Date(release_date),
+      },
+    });
+  } catch (error) {
+    res.status(500).send({ message: "Falha ao cadastrar um filme" });
+  }
   res.status(201).send();
+});
+
+app.put("/movies/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  try {
+    const movie = await prisma.movie.findUnique({
+      where: { id },
+    });
+    if (!movie) {
+      res.status(404).send({ message: "Filme não encontrado" });
+    } 
+    const data = { ...req.body };
+    data.release_date = data.release_date
+      ? new Date(data.release_date)
+      : undefined;
+    await prisma.movie.update({ where: { id }, data });
+  } catch (error) {
+    res.status(500).send({ message: "Falha ao atualizar o registro" });
+  }
 });
 
 app.listen(port, () => {
