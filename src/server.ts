75147de54c5f5cb6048f25a400a84b42bff799fd
Tauan Mayk <tauanmaykreal@gmail.com>
2025-05-15 20:14:50 -0300

    adicionando a documentação do swagger.json e a documentação inicial

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
-import { Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 const prisma = new PrismaClient();
 const port = 3000;
 const app = express();
 
 app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.get("/movies", async (_, res) => {
   const movies = await prisma.movie.findMany({
